ee8a424f39105682b41b9f38270db26b307c6821
Keyur <Keyurdhanani456@gmail.com>
2024-04-13 01:30:38 +0530

    Add Group Done

---
+++ medico-frontend/src/Components/GroupCompenents/CaseOne.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+import { CustomInput, CustomTextArea } from "../OfferComponents/Input";
+
+import { getDistricts, getStates, getTalukas } from "../../Services/location";
+import GroupLocationBox from "./GroupLocationBox";
+
+function SelectLocation({ register, errors, setsTaluka }) {
+  const [LoadDistrict, setLoadDistrict] = useState(false);
+  const [LoadTaluka, setLoadTaluka] = useState(false);
+
+  const [states, setStates] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [talukas, setTalukas] = useState([]);
+
+  const [distActive, setdistActive] = useState(false);
+  const [talukaActive, settalukaActive] = useState(false);
+
+  const [selectedTaluka, setselectedTaluka] = useState([]);
+
+  // console.log(selectedTaluka);
+
+  useEffect(() => {
+    getStates()
+      .then((response) => {
+        console.log(response);
+        setStates(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    setsTaluka(selectedTaluka);
+  }, [selectedTaluka]);
+
+  const getDistrictsbyState = async (states) => {
+    setLoadDistrict(true);
+    setDistricts([]);
+    try {
+      await Promise.all(
+        states.map(async (s, i) => {
+          try {
+            const resp = await getDistricts(s);
+            setDistricts((prevDistricts) => [...prevDistricts, ...resp]);
+            if (i === states.length - 1) {
+              setLoadDistrict(false);
+              setdistActive(true);
+            }
+            console.log(districts);
+          } catch (err) {
+            console.log(err);
+            setLoadDistrict(false);
+          }
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTalukasByDistrict = async (districts) => {
+    setLoadTaluka(true);
+    setTalukas([]);
+    try {
+      let temp = [];
+      await Promise.all(
+        districts.map(async (s) => {
+          try {
+            const resp = await getTalukas(s);
+            temp.push(...resp);
+          } catch (err) {
+            console.log(err);
+          }
+        })
+      );
+      console.log(temp);
+      setTalukas(temp);
+      setLoadTaluka(false);
+      settalukaActive(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <fieldset className="">
+        <div className="flex flex-col md:flex-row items-center gap-6">
+          <div>
+            <CustomInput
+              placeholder="Enter Group Name"
+              inputProps={register("groupName", {
+                required: "Group Nameis required",
+              })}
+              error={errors.groupName}
+            />
+          </div>
+          <div>
+            <CustomTextArea
+              placeholder="Enter Group Description"
+              inputProps={register("groupDescription", {
+                required: "Group Description is required",
+              })}
+              small={true}
+              error={errors.groupDescription}
+            />
+          </div>
+        </div>
+        <div className="mt-6">
+          <GroupLocationBox
+            loadNext={getDistrictsbyState}
+            isLoading={LoadDistrict}
+            data={states}
+            next={"Districts"}
+            curr={"State"}
+            setDistActive={setdistActive}
+            setTalukaActive={settalukaActive}
+          />
+        </div>
+        <div className="mt-6">
+          {distActive && (
+            <GroupLocationBox
+              isLoading={LoadTaluka}
+              data={districts}
+              next={"Talukas"}
+              loadNext={getTalukasByDistrict}
+              curr={"District"}
+              setTalukaActive={settalukaActive}
+            />
+          )}
+        </div>
+        <div className="mt-6">
+          {talukaActive && (
+            <GroupLocationBox
+              isLoading={LoadTaluka}
+              data={talukas}
+              curr={"Taluka"}
+              next={""}
+              setselectedTaluka={setselectedTaluka}
+            />
+          )}
+        </div>
+      </fieldset>
+    </>
+  );
+}
+
+export default SelectLocation;
---
+++ medico-frontend/src/Components/GroupCompenents/CaseTwo.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { CustomSelect, CustomCheckbox } from "../OfferComponents/Input";
+
+function CaseTwo({ register, errors, occupation }) {
+  // console.log(occupation);
+  return (
+    <div className="flex flex-col gap-8 text-white">
+      <div className="flex gap-8">
+        <label className="flex items-center space-x-2 text-lg">
+          <input
+            type="checkbox"
+            value="Doctor"
+            className="form-checkbox h-4 w-4"
+            {...register("occupation")}
+          />
+          <span>Doctor</span>
+        </label>
+        <label className="flex items-center space-x-2 text-lg">
+          <input
+            type="checkbox"
+            value="Medical Store"
+            className="form-checkbox h-4 w-4"
+            {...register("occupation")}
+          />
+          <span>Medical Store</span>
+        </label>
+      </div>
+      {occupation && occupation.includes("Doctor") && (
+        <>
+          <p className=" text-2xl"> Select Degree</p>
+          <div className="grid grid-cols-4 gap-8">
+            <label className="flex items-center space-x-2 text-lg">
+              <input
+                type="checkbox"
+                value="Master of Surgery"
+                className="form-checkbox h-4 w-4"
+                {...register("degree")}
+              />
+              <span>Master of Surgery</span>
+            </label>
+
+            <label className="flex items-center space-x-2 text-xl">
+              <input
+                type="checkbox"
+                value="Bachelor of Medicine Bachelor of Surgery"
+                className="form-checkbox h-4 w-4"
+                {...register("degree")}
+              />
+              <span>Bachelor of Medicine Bachelor of Surgery</span>
+            </label>
+
+            <label className="flex items-center space-x-2 text-xl">
+              <input
+                type="checkbox"
+                value="Bachelor of Dental Surgery"
+                className="form-checkbox h-4 w-4"
+                {...register("degree")}
+              />
+              <span>Bachelor of Dental Surgery</span>
+            </label>
+
+            <label className="flex items-center space-x-2 text-xl">
+              <input
+                type="checkbox"
+                value="Bachelor of Homeopathy Medicine and Surgery"
+                className="form-checkbox h-4 w-4"
+                {...register("degree")}
+              />
+              <span>Bachelor of Homeopathy Medicine and Surgery</span>
+            </label>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default CaseTwo;
