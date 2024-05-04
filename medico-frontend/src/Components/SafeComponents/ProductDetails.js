30b1de727d384d1c39bf851802bbb689b562b0d8
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2024-05-04 22:01:04 +0530

    updated product detail page

--- medico-frontend/src/Components/SafeComponents/ProductDetails.js
+++ medico-frontend/src/Components/SafeComponents/ProductDetails.js
@@ -75,7 +75,7 @@ const AddProductDetailModal = ({
 
   return (
     <div
-      className={`fixed inset-0  flex items-center justify-center bg-black  ${
+      className={`fixed inset-0  flex items-center justify-center bg-black bg-opacity-75  ${
         isOpen ? "" : "hidden"
       }`}
     >
