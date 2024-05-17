2ffe5d6cb8d909928526a68df8725370e41509df
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2024-05-17 23:56:11 +0530

    offers show and delete

--- medico-frontend/src/Services/offer.js
+++ medico-frontend/src/Services/offer.js
@@ -124,3 +124,26 @@ export const getOffers = async (email) => {
       throw error; // Throw error for further handling if needed
     });
 };
+
+export const getOfferById = async (offerId) => {
+  const t = localStorage.getItem("token");
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${t}`,
+    },
+  };
+
+  return axios
+    .get(url + `/api/offer/${offerId}`, config)
+    .then((response) => {
+      // Handle successful response
+      return response.data; // Return data if needed
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error occurred during login:", error);
+      throw error; // Throw error for further handling if needed
+    });
+};
