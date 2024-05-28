fd0e3a07e2bc5d168589ffaff388a499db8187ad
Keyur <Keyurdhanani456@gmail.com>
2024-05-29 00:02:38 +0530

    Buyer Home page

---
+++ medico-frontend/src/Components/Buyer/BuyerSlider.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { GrNext, GrPrevious } from "react-icons/gr";
+
+export default function BuyerSlider({ images }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  };
+
+  return (
+    <div className="container mx-auto mt-4">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="w-full mx-auto h-80"
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
