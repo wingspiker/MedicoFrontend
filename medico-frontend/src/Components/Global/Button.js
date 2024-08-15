369947e4d4b55a5b67c58b9d0336957900233703
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-15 22:05:04 +0530

    feat: new admin login

--- medico-frontend/src/Components/Global/Button.js
+++ medico-frontend/src/Components/Global/Button.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { cn } from "../../lib/cn";
 
-export default function Button() {
-  return <div>Button</div>;
+export default function Button({ children, className, ...props }) {
+  return (
+    <button
+      {...props}
+      className={cn(
+        `flex items-center justify-center bg-cyan-600 text-white px-4 py-2 rounded-xl`,
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
 }
